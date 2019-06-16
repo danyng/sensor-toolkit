c80f85fff85f913dde218c7dd3c8a689303d916e
danyng <48064715+danyng@users.noreply.github.com>
2019-06-16 15:59:34 +0200

    Add files via upload

---
+++ ESP8266/module_brightness/module_brightness.ino
@@ -0,0 +1,49 @@
+/* brightness module 
+ * sending brightness level every time interval
+*/
+
+
+#include "OOCSI.h"
+
+const char* ssid = "STK LOCAL NAME"; 
+const char* password = "STK LOCAL PASSWORD";
+
+const char* OOCSIName = "module_microphone";
+const char* recipient = "stk_demo_visualizer";
+
+const char* hostserver = "INSERT IP HERE";
+const char* key = "brightnessVal";
+
+OOCSI oocsi = OOCSI();
+
+
+#define ledPin D4
+int timeInterval = 1000;
+
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);    // LOW = on
+  
+  oocsi.connect(OOCSIName, hostserver, ssid, password, processOOCSI);
+}
+
+
+
+void loop() {
+
+  oocsi.newMessage(recipient);
+  oocsi.addInt(key, analogRead(A0));
+  oocsi.sendMessage();
+
+  oocsi.check();
+  delay(timeInterval);
+}
+
+
+
+void processOOCSI() {
+  // don't do anything; we are sending only
+}
